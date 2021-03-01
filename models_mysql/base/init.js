18544ca584c79e420eab453e560ff19df4491ebc
zhoufei <1174977961@qq.com>
2021-03-01 12:24:19 +0800

    feat: 报错打印

--- models_mysql/base/init.js
+++ models_mysql/base/init.js
@@ -11,14 +11,19 @@ let pool =mysql.createPool(env);
 
 exports.do = function (sql, callback){
     this.getConnection(function (err, connection){
-        console.log('sql: ',sql)
-        connection.query(sql, function (){
+        console.log("sql: ",sql)
+        if (err) {
+            console.log("err: ",err)
+        } else {
+            connection.query(sql, function (){
 
-            callback.apply(connection, arguments);
+                callback.apply(connection, arguments);
 
-            connection.release();
+                connection.release();
 
-        });
+            });
+        }
+        
 
     })
 
